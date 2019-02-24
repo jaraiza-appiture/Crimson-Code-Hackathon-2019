03c0e1c5ed57301bd10866bb87e999e33c4883e0
william Deleon <william.deleon@wsu.edu>
2019-02-24 01:22:49 -0800

    SLow down

--- move.py
+++ move.py
@@ -3,7 +3,7 @@ import time
 import sys
 
 pi = pigpio.pi()
-
+pi.set_mode(23, pigpio.OUTPUT)
 # PWM 1/4 on
 pi.set_PWM_dutycycle(18, 1)
 pi.set_PWM_dutycycle(13, 1)
@@ -11,14 +11,15 @@ pi.set_PWM_dutycycle(13, 1)
 
 try:
     while True:
+        pi.set_mode(32, pigpio.LOW) # turn off LED
         direction = input('enter direction:')
         # sys.argv[1]
         # distance_x = sys.argv[2]
         # distance_y = sys.argv[3]
 
         if direction == "f":
-            pi.set_servo_pulsewidth(18, 1520)
-            pi.set_servo_pulsewidth(13, 1355)
+            pi.set_servo_pulsewidth(18, 1525)
+            pi.set_servo_pulsewidth(13, 1350)
         elif direction == "l":
             pi.set_servo_pulsewidth(18, 1490)
             pi.set_servo_pulsewidth(13, 1365)
@@ -26,6 +27,7 @@ try:
             pi.set_servo_pulsewidth(18, 1510)
             pi.set_servo_pulsewidth(13, 1380)
         elif direction == "s":
+            pi.set_mode(32, pigpio.HIGH) # turn on LED
             pi.set_servo_pulsewidth(18, 1500)
             pi.set_servo_pulsewidth(13, 1370)
         else:
