8fc7edd85caaa01e0f5f7228e953c7e6a7b7c6c8
william Deleon <william.deleon@wsu.edu>
2019-02-23 21:40:23 -0800

    My last hope

--- test.py
+++ test.py
@@ -4,11 +4,18 @@ import time
 
 pi = pigpio.pi()
 
+pi.set_mode(12, pigpio.OUTPUT)
+
+print("no movement")
 pi.set_servo_pulsewidth(13, 0)
 time.sleep(1)
 
+print("CCW")
 pi.set_servo_pulsewidth(13, 1000)
+
+print("CW")
 pi.set_servo_pulsewidth(13, 2000)
+
+print("stop")
 pi.set_servo_pulsewidth(13, 0)
 
-pi.stop()
