d24115cf558f6189e6efe93a93fc207bbc8fd7b6
william Deleon <william.deleon@wsu.edu>
2019-02-23 22:27:52 -0800

    test

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ pi = pigpio.pi()
 
 pi.set_servo_pulsewidth(13, 1400)
 
-for i in range(1300, 1400, 10):
+for i in range(1360, 1380, 5):
     print ("PW = ", i)
     pi.set_servo_pulsewidth(13, i)
     time.sleep(2)
