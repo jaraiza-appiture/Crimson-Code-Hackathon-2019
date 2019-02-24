589cfddd827daeb862aa5fdd25a28383607abea9
william Deleon <william.deleon@wsu.edu>
2019-02-24 00:03:07 -0800

    SLow down

--- move.py
+++ move.py
@@ -20,8 +20,8 @@ try:
             pi.set_servo_pulsewidth(18, 1520)
             pi.set_servo_pulsewidth(13, 1355)
         elif direction == "l":
-            pi.set_servo_pulsewidth(18, 1450)
-            pi.set_servo_pulsewidth(13, 1330)
+            pi.set_servo_pulsewidth(18, 1460)
+            pi.set_servo_pulsewidth(13, 1340)
         elif direction == "r":
             pi.set_servo_pulsewidth(18, 1500)
             pi.set_servo_pulsewidth(13, 1370)
