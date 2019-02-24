229531f812f604d73ec95e4e5a04461517bc1467
william Deleon <william.deleon@wsu.edu>
2019-02-23 22:15:54 -0800

    test

--- test.py
+++ test.py
@@ -10,14 +10,3 @@ pi.set_servo_pulsewidth(18, 1500)
 time.sleep(30)
 
 
-try:
-    while 1:
-        print("Enter direction: ")
-        movement = input()
-
-        if movement == "l":
-            pi.set_servo_pulsewidth(18, )
-
-except KeyboardInterrupt:
-    pi.stop()
-
