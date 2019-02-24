cf9deb06f63cab03d685bb22bc8a52aedbec416d
william Deleon <william.deleon@wsu.edu>
2019-02-23 23:38:15 -0800

    SLow down

--- move.py
+++ move.py
@@ -5,8 +5,8 @@ import sys
 pi = pigpio.pi()
 
 # PWM 1/4 on
-pi.set_PWM_dutycycle(18, 20)
-pi.set_PWM_dutycycle(13, 20)
+pi.set_PWM_dutycycle(18, 1)
+pi.set_PWM_dutycycle(13, 1)
 
 
 try:
@@ -17,17 +17,17 @@ try:
         # distance_y = sys.argv[3]
 
         if direction == "f":
-            pi.set_servo_pulsewidth(18, 1600)
-            pi.set_servo_pulsewidth(13, 1000)
+            pi.set_servo_pulsewidth(18, 1550)
+            pi.set_servo_pulsewidth(13, 1300)
         elif direction == "b":
-            pi.set_servo_pulsewidth(18, 1000)
-            pi.set_servo_pulsewidth(13, 1600)
+            pi.set_servo_pulsewidth(18, 1300)
+            pi.set_servo_pulsewidth(13, 1550)
         elif direction == "l":
-            pi.set_servo_pulsewidth(18, 1600)
-            pi.set_servo_pulsewidth(13, 1600)
+            pi.set_servo_pulsewidth(18, 1400)
+            pi.set_servo_pulsewidth(13, 1400)
         elif direction == "r":
-            pi.set_servo_pulsewidth(18, 500)
-            pi.set_servo_pulsewidth(13, 500)
+            pi.set_servo_pulsewidth(18, 1400)
+            pi.set_servo_pulsewidth(13, 1400)
         elif direction == "s":
             pi.set_servo_pulsewidth(18, 1500)
             pi.set_servo_pulsewidth(13, 1370)
