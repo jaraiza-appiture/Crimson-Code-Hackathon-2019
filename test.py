3ff308f39a12d3db0ad81010b5bb3eb17e3131d1
william Deleon <william.deleon@wsu.edu>
2019-02-23 16:23:25 -0800

    no pulse to servos test 10

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ import RPi.GPIO as GPIO
 import time
 
 # Constant values
-STOPVAL = 95 # Duty Cycle that stops servo
+STOP = 95 # Duty Cycle that stops servo
 PWM0 = 12
 PWM1 = 33
 FREQ = 100 # 100Hz
+CCW = 50
+CW = 85
 
 # variables
 i = 0
@@ -14,22 +16,41 @@ i = 0
 # setup pin number based off board numbering
 GPIO.setmode(GPIO.BOARD)
 
-# setup pin 12 to be an output pin for PWM0
+# setup pin 12 & 33 to  be an output pin for PWM0 and PWM1
 GPIO.setup(PWM0, GPIO.OUT)
+GPIO.setup(PWM1, GPIO.OUT)
 
-# PWM instance associated GPIO Pin 12 at 50Hz
-p = GPIO.PWM(PWM0, FREQ)
+# PWM instance associated GPIO Pin 12 and 33 at 100 HZ
+p0 = GPIO.PWM(PWM0, FREQ)
+p1 = GPIO.PWM(PWM1, FREQ)
 
 # start pulse at Duty cycle=i
-p.start(i)
+p0.start(i)
+p1.start(i)
 
 # ********************* Main Code *****************
 try:
     while True:
-        for i in range(0, 101, 1):
-            print("DC = %d", i)
-            p.ChangeDutyCycle(i)
-            time.sleep(1.5)
+        print("enter direction:")
+        x = input()
+
+        if x == "left":
+            p0.ChangeDutyCycle(CCW)
+            p1.ChangeDutyCycle(CW)
+        elif x == "right":
+            p0.ChangeDutyCycle(CW)
+            p1.ChangeDutyCycle(CCW)
+        elif x == "forward":
+            p0.ChangeDutyCycle(CCW)
+            p1.ChangeDutyCycle(CCW)
+        elif x == "backward":
+            p0.ChangeDutyCycle(CW)
+            p1.ChangeDutyCycle(CW)
+        else:
+            p0.ChangeDutyCycle(STOP)
+            p1.ChangeDutyCycle(STOP)
+
 except KeyboardInterrupt:
-    p.stop()
+    p0.stop()
+    p1.stop()
     GPIO.cleanup()
