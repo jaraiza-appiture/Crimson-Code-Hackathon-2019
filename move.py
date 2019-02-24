6e418526e5e33ec5ef6eb432da6eb435cc84e649
william Deleon <william.deleon@wsu.edu>
2019-02-23 23:47:05 -0800

    SLow down

--- move.py
+++ move.py
@@ -18,7 +18,7 @@ try:
 
         if direction == "f":
             pi.set_servo_pulsewidth(18, 1480)
-            pi.set_servo_pulsewidth(13, 1360)
+            pi.set_servo_pulsewidth(13, 1400)
         elif direction == "l":
             pi.set_servo_pulsewidth(18, 1500)
             pi.set_servo_pulsewidth(13, 1370)
