4d1dce8c70938f8beab8283329b4ec276bf72bbe
william Deleon <william.deleon@wsu.edu>
2019-02-23 21:10:33 -0800

    My last hope

--- test.py
+++ test.py
@@ -8,24 +8,4 @@ pi.set_servo_pulsewidth(18, 0)
 pi.set_servo_pulsewidth(13, 0)
 time.sleep(5)
 
-pi.set_servo_pulsewidth(18, 1500)  # GPIO18 (PWM0) and 1.5ms
-pi.set_servo_pulsewidth(13, 1500)  # GPIO13 (PWM1) and 1.5ms
-
-time.sleep(1)
-pi.set_servo_pulsewidth(18, 1000)
-pi.set_servo_pulsewidth(13, 1000)
-time.sleep(1)
-
-pi.set_servo_pulsewidth(18,1500)
-pi.set_servo_pulsewidth(13,1500)
-time.sleep(1)
-
-pi.set_servo_pulsewidth(18, 1500)
-pi.set_servo_pulsewidth(13, 2000)
-time.sleep(1)
-
-pi.set_servo_pulsewidth(18, 0)
-pi.set_servo_pulsewidth(13, 0)
-time.sleep(5)
-
 pi.stop()
