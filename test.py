7d892293a52bb3965fa848a03103578aef233989
william Deleon <william.deleon@wsu.edu>
2019-02-23 22:15:40 -0800

    test

--- test.py
+++ test.py
@@ -5,9 +5,19 @@ import time
 pi = pigpio.pi()
 
 
-pi.set_servo_pulsewidth(13, 1500)
-pi.set_servo_pulsewidth(18, 1400)
+pi.set_servo_pulsewidth(13, 1400)
+pi.set_servo_pulsewidth(18, 1500)
 time.sleep(30)
 
 
+try:
+    while 1:
+        print("Enter direction: ")
+        movement = input()
+
+        if movement == "l":
+            pi.set_servo_pulsewidth(18, )
+
+except KeyboardInterrupt:
+    pi.stop()
 
