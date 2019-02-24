bbc777928b267861c4379bc8c48d7ea90dcf92e3
william Deleon <william.deleon@wsu.edu>
2019-02-23 20:04:58 -0800

    My last hope

--- move.py
+++ move.py
@@ -10,13 +10,13 @@ wiringpi.pwmWrite(12, 0)  # duty cycle between 0 and 1024. 0 = off, 1024 = fully
 pause_time = 0.002          # you can change this to slow down/speed up
 
 try:
-while True:
-    for i in range(0, 1025):  # 1025 because it stops at 1024
-        wiringpi.pwmWrite(18, i)
-        sleep(pause_time)
-    for i in range(1024, -1, -1):  # from 1024 to zero in steps of -1
-        wiringpi.pwmWrite(18, i)
-        sleep(pause_time)
+    while True:
+        for i in range(0, 1025):  # 1025 because it stops at 1024
+            wiringpi.pwmWrite(18, i)
+            sleep(pause_time)
+        for i in range(1024, -1, -1):  # from 1024 to zero in steps of -1
+            wiringpi.pwmWrite(18, i)
+            sleep(pause_time)
 
 finally:
 wiringpi.pwmWrite(18, 0)  # switch PWM output to 0
