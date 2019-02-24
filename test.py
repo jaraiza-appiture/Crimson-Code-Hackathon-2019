929542e060b31030c71dc7808a1a05476230c3bd
william Deleon <william.deleon@wsu.edu>
2019-02-23 21:03:13 -0800

    My last hope

--- test.py
+++ test.py
@@ -4,11 +4,14 @@ import time
 
 pi = pigpio.pi()
 
+pi.set_servo_pulsewidth(18, 0)
+pi.set_servo_pulsewidth(13, 0)
+time.sleep(5)
+
 pi.set_servo_pulsewidth(18, 1500)  # GPIO18 (PWM0) and 1.5ms
 pi.set_servo_pulsewidth(13, 1500)  # GPIO13 (PWM1) and 1.5ms
 
 time.sleep(1)
-
 pi.set_servo_pulsewidth(18, 1000)
 pi.set_servo_pulsewidth(13, 1000)
 time.sleep(1)
@@ -17,5 +20,8 @@ pi.set_servo_pulsewidth(18,1500)
 pi.set_servo_pulsewidth(13,1500)
 time.sleep(1)
 
-pi.set_servo_pulsewidth(18, 0)
-pi.set_servo_pulsewidth(13, 0)
+pi.set_servo_pulsewidth(18, 1500)
+pi.set_servo_pulsewidth(18, 2000)
+time.sleep(1)
+
+pi.stop()
