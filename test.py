0780cebf888199ed9fba8bcf46cb0d30d556b6c1
william Deleon <william.deleon@wsu.edu>
2019-02-23 21:56:37 -0800

    test

--- test.py
+++ test.py
@@ -6,21 +6,8 @@ pi = pigpio.pi()
 
 pi.set_mode(12, pigpio.OUTPUT)
 
-print("no movement")
-pi.set_servo_pulsewidth(18, 0)
-time.sleep(1)
-
-print("CCW")
-pi.set_servo_pulsewidth(18, 2000)
-time.sleep(3)
-
-print("CW")
-pi.set_servo_pulsewidth(18, 1000)
-time.sleep(3)
-
 print("stop")
 pi.set_servo_pulsewidth(18, 1500)
-pi.set_PWM_dutycycle(18, 0)
-time.sleep(3)
+time.sleep(10)
 
 
