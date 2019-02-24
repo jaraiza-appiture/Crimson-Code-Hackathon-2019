3521c9c7afd1ecd129c3997e7a47e53b957595bd
william Deleon <william.deleon@wsu.edu>
2019-02-23 21:46:03 -0800

    My last hope

--- test.py
+++ test.py
@@ -7,15 +7,18 @@ pi = pigpio.pi()
 pi.set_mode(12, pigpio.OUTPUT)
 
 print("no movement")
-pi.set_servo_pulsewidth(13, 0)
+pi.set_servo_pulsewidth(18, 0)
 time.sleep(1)
 
 print("CCW")
-pi.set_servo_pulsewidth(13, 1000)
+pi.set_servo_pulsewidth(18, 1000)
+time.sleep(1)
 
 print("CW")
-pi.set_servo_pulsewidth(13, 2000)
+pi.set_servo_pulsewidth(18, 2000)
+time.sleep(1)
 
 print("stop")
-pi.set_servo_pulsewidth(13, 0)
+pi.set_servo_pulsewidth(18, 0)
+time.sleep(1)
 
