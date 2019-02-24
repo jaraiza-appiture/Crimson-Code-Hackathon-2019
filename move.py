293b5e406f838e2124d1c4f025e37105dcc7db06
william Deleon <william.deleon@wsu.edu>
2019-02-23 20:58:59 -0800

    My last hope

--- move.py
+++ move.py
@@ -1,23 +1,21 @@
-import wiringpi
+import RPi.GPIO as GPIO
+import time as time
 
-wiringpi.piBoardRev()
+GPIO.setmode(GPIO.Board)
+GPIO.setup(18, GPIO.OUT)
 
-# OR, using P1 header pin numbers
-wiringpi.wiringPiSetupPhys()
-wiringpi.pinMode(12, 2)  # pwm only works on P1 header pin 12
-wiringpi.pwmWrite(12, 0)  # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
-
-pause_time = 0.002          # you can change this to slow down/speed up
-
+servo = GPIO.PWM(18, 500)
+servo.start(0)
 try:
     while True:
-        for i in range(0, 1025):  # 1025 because it stops at 1024
-            wiringpi.pwmWrite(18, i)
-            sleep(pause_time)
-        for i in range(1024, -1, -1):  # from 1024 to zero in steps of -1
-            wiringpi.pwmWrite(18, i)
-            sleep(pause_time)
-
-finally:
-    wiringpi.pwmWrite(18, 0)  # switch PWM output to 0
-    wiringpi.pinMode(18, 0)  # GPIO18 to input
+        for dc in range(50, 101, 5):
+            servo.ChangeDutyCycle(dc)
+            servo.ChangeDutyCycle(dc)
+            time.sleep(0.5)
+        for dc in range(100, 45, -5):
+            servo.ChangeDutyCycle(dc)
+            time.sleep(0.5)
+except KeyboardInterrupt:
+    pass
+servo.stop()
+GPIO.cleanup();
--- test.py
+++ test.py
@@ -1,72 +1,21 @@
-# External module imports
 import RPi.GPIO as GPIO
+import pigpio
 import time
 
-# Constant values
-STOP = 95  # Duty Cycle that stops servo
-PWM0 = 12
-PWM1 = 33
-FREQ = 1000  # 100Hz
-CCW = 80
-CW = 85
+pi = pigpio.pi()
 
+pi.set_servo_pulsewidth(18, 1500)  # GPIO18 (PWM0) and 1.5ms
+pi.set_servo_pulsewidth(13, 1500)  # GPIO13 (PWM1) and 1.5ms
 
-def connection(self):
-    GPIO.setup(PWM0, GPIO.OUT)
-    GPIO.setup(PWM1, GPIO.OUT)
+time.sleep(1)
 
-    # PWM instance associated GPIO Pin 12 and 33 at 100 HZ
-    p0 = GPIO.PWM(PWM0, FREQ)
-    p1 = GPIO.PWM(PWM1, FREQ)
+pi.set_servo_pulsewidth(18, 1000)
+pi.set_servo_pulsewidth(13, 1000)
+time.sleep(1)
 
-# variables
+pi.set_servo_pulsewidth(18,1500)
+pi.set_servo_pulsewidth(13,1500)
+time.sleep(1)
 
-i = 0
-
-# setup pin number based off board numbering
-GPIO.setmode(GPIO.BOARD)
-
-# setup pin 12 & 33 to  be an output pin for PWM0 and PWM1
-GPIO.setup(PWM0, GPIO.OUT)
-GPIO.setup(PWM1, GPIO.OUT)
-
-# PWM instance associated GPIO Pin 12 and 33 at 100 HZ
-p0 = GPIO.PWM(PWM0, FREQ)
-p1 = GPIO.PWM(PWM1, FREQ)
-
-# start pulse at Duty cycle=i
-p0.start(i)
-p1.start(i)
-
-# ********************* Main Code *****************
-try:
-    while True:
-        print("enter direction:")
-        x = input()
-
-        if x == "left":
-            p0.ChangeDutyCycle(7.5)
-            p1.ChangeDutyCycle(7.5)
-        elif x == "right":
-            p0.ChangeDutyCycle(7.5)
-            p1.ChangeDutyCycle(7.5)
-        elif x == "forward":
-            p0.ChangeDutyCycle(CCW)
-            p1.ChangeDutyCycle(CW)
-        elif x == "backward":
-            p0.ChangeDutyCycle(CW)
-            p1.ChangeDutyCycle(CCW)
-        elif x == "fix":
-            for i in range(0, 101, 1):
-                p0.ChangeDutyCycle(i)
-                p1.ChangeDutyCycle(i)
-                time.sleep(1.5)
-        else:
-            p0.ChangeDutyCycle(0)
-            p1.ChangeDutyCycle(0)
-
-except KeyboardInterrupt:
-    p0.stop()
-    p1.stop()
-    GPIO.cleanup()
-
+pi.set_servo_pulsewidth(18, 0)
+pi.set_servo_pulsewidth(13, 0)
